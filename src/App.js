22c2905f71d42364b1f3a56f49a26304781f6b37
Sylvester Wamaya <106195171+sylvester-wamaya@users.noreply.github.com>
2022-12-02 23:53:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Watched from './components/Watched';
 import Watchlist from './components/Watchlist';
 import Add from './components/Add';
+import Add from './components/Add';
 import './App.css';
 import "./lib/font-awesome/css/all.min.css"
 import { GlobalProvider } from './context/GlobalState';
@@ -17,6 +18,7 @@ function App() {
         <Route exact path='/' element={<Watchlist/>}/>
         <Route path='/wathed' element={<Watched/>}/>
         <Route path='/add' element={<Add/>}/>
+        <Route path='*' element={<Add/>}/>
         </Routes>
     
     </Router>
